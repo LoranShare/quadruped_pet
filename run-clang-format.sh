662ed65da621853d44ec183878e046f623630ee3
LoranShare <janik9309@mail.ru>
2023-11-27 21:19:33 +0300

    feat: added clang format configuration and launch script
    Task: #7

---
+++ run-clang-format.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+find software/tests/test/ -type f \( -name \*.c -o -name \*.h \) -exec clang-format-16 --verbose --style=file -i '{}' \;
+find Src/ -maxdepth 1 -type f \( -name \*.c -o -name \*.h \) -exec clang-format-16 --verbose --style=file -i '{}' \;
